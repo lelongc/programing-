34db095e01ca415c932e918ba50db67083ba825f
Shima_Rin <lelong190110gmail.com>
2025-06-14 14:38:25 +0700

    2382025pm

---
+++ hoi-dan-it/01/nodejs/src/controllers/users.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import handleCreateUser from "../services/user-service";
+interface HomePageData {
+  title: string;
+}
+
+const getHomepage = (req: Request, res: Response): void => {
+  return res.render("home", {
+    title: "meo con",
+  } as HomePageData);
+  // Hàm getHomepage nhận vào hai tham số req và res, đại diện cho yêu cầu và phản hồi HTTP.
+  // Hàm này sẽ trả về một trang web với tiêu đề "meo" bằng cách sử dụng phương thức render của đối tượng res.
+};
+// Hàm getHomepage sẽ được sử dụng trong file web.ts để xử lý yêu cầu GET đến trang chủ.
+const getCreateUserPage = (req: Request, res: Response): void => {
+  return res.render("create-user", {
+    title: " User",
+  } as HomePageData);
+};
+
+const postCreateUserPage = (req: Request, res: Response): void => {
+  // Lấy dữ liệu từ form gửi lên
+  const { username, email, address } = req.body;
+  handleCreateUser(username, email, address);
+  return res.redirect("/");
+};
+export { getHomepage, getCreateUserPage, postCreateUserPage };
