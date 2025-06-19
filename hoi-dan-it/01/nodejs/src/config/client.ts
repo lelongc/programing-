471ec6ccedc436ebcc17d0d7a04c351685d2f534
Shima_Rin <lelong190110gmail.com>
2025-06-19 13:41:49 +0700

    feat: Update environment configuration for Prisma and enhance logging setup

--- hoi-dan-it/01/nodejs/src/config/client.ts
+++ hoi-dan-it/01/nodejs/src/config/client.ts
@@ -1,8 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 // This is a workaround to avoid creating multiple PrismaClient instances in development mode
 // It ensures that the PrismaClient is only instantiated once, preventing issues with hot reloading in development
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma = globalForPrisma.prisma || new PrismaClient(
+      {
+            log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
+      }// This sets the logging level for PrismaClient based on the environment 
+      // In development, it logs queries, info, warnings, and errors
+      // In production, it only logs errors
+)
 // If the globalForPrisma.prisma is not defined, create a new PrismaClient instance
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
